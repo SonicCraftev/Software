a88d236eb9eb97b5fd2971e8006a74d1837ddadb
SonicCraftev <146214348+SonicCraftev@users.noreply.github.com>
2024-03-09 20:00:15 +0100

    responses_OS.py

--- responses_OS.py
+++ responses_OS.py
@@ -1,5 +1,1 @@
-responses_data = (["Update and record research on time",
-             "Update and record on paradoxes",
-             "Complete research on The Egg Theory",
-             "Complete research on The black swan theory",
-             "Minecraft", "Boywithuke"])
+responses_data = (["In put any response"])
